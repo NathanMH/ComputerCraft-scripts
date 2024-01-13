cf1db151e1d9678d16ce07958b0d650b718592b9
MusicNate <nathan@musicnate.ca>
2024-01-13 15:06:36 -0500

    Add comments to farming script

--- farm-melon-wheat.lua
+++ farm-melon-wheat.lua
@@ -1,17 +1,17 @@
+--[[
+This program is good for wheat, flax and melons/pumpkins and will drop items into a chest above the turtle
+--]]
+
+-- Drops all items into a chest/drawer controller above the turtle
 local function dropAll()
-    for i=2,16 do
+    for i=4,16 do
         turtle.select(i)
-        turtle.drop()
+        turtle.dropUp()
     end
     turtle.select(1)
 end
 
-local function flax()
-    turtle.dig()
-    turtle.select(1)
-    turtle.place()
-end
-
+-- Wheat is in slot 1
 local function wheat()
     local success, result = turtle.inspect()
     if result.state.age == 7 then
@@ -21,20 +21,26 @@ local function wheat()
     end
 end
 
+-- Flax is in slot 2
+local function flax()
+    turtle.dig()
+    turtle.select(2)
+    turtle.place()
+end
 
+-- Main routine
 while true do
 
     local success, result = turtle.inspect()
-    if success and result.name == "minecraft:wheat" then 
+    if success and result.name == "minecraft:wheat" then
         wheat()
     elseif result.name == "minecraft:melon" or result.name == "minecraft:pumpkin" then
         turtle.dig()
-    elseif result.name == "thermal:flax" then 
+    elseif result.name == "thermal:flax" then
         if result.state.age == 6 then
             flax()
         end
-    elseif result.name == "storagedrawers:controller" then
-        dropAll()
     end
-        turtle.turnRight()
+    turtle.turnRight()
+    dropAll()
 end
